313101cf560a56775588ac4892c840725fd6377d
Vidit Maheshwari <viditmaheshwari@CITRUSs-MacBook-Pro-2.local>
2016-08-29 10:35:48 +0530

    first commit

---
+++ login/loginPage/models.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils import timezone
+
+import datetime
+
+
+# Create your models here.
+
+@python_2_unicode_compatible
+class UserDetail(models.Model):
+	login_id = models.CharField(max_length=256)
+	login_challenge = models.CharField(max_length=256)
+	created = models.DateTimeField('date published')
+
+	def __str__(self):
+		return self.login_id
+
+	def is_created_5days_ago(self):
+		return self.created >= timezone.now() - datetime.timedelta(days=5)
